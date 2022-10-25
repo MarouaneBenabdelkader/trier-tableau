31318a700913274a0b6d0edd7e7c273aa2d46247
MarouaneBenbadelkader <marouane.benabdelkader@usmba.ac.ma>
2022-10-25 10:31:35 +0100

    Add project files.

---
+++ trier tableau/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace trier_tableau
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tab = new int[10];
+            tab[0] = 5; 
+            tab[1] = 1;
+            tab[2] = 10;
+            tab[3] = 4;
+            Array.Sort(tab);
+            int number;
+            int tmp;
+            number = Console.Read();
+            for (int i = 0; i < tab.Length; i++)
+            {
+                if(number > tab[i])
+                {
+                    for (int j = i; j < tab.Length; j++)
+                    {
+                        tmp = tab[j];
+
+                    }
+                    
+                }
+            }
+            for (int i = 0; i < tab.Length; i++)
+            {
+                Console .WriteLine(tab[i]);
+            }
+
+        }
+    }
+}
