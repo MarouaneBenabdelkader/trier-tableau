83da9d4e65b6113471c6b2dd53d292018561f549
MarouaneBenbadelkader <marouane.benabdelkader@usmba.ac.ma>
2022-10-25 11:53:28 +0100

    add element

--- trier tableau/Program.cs
+++ trier tableau/Program.cs
@@ -5,28 +5,37 @@ namespace trier_tableau
     {
         static void Main(string[] args)
         {
-            int[] tab = new int[10];
-            tab[0] = 5; 
+            int[] tab = new int[6];
+            tab[0] = 11; 
             tab[1] = 1;
             tab[2] = 10;
             tab[3] = 4;
+            tab[4] = 7;
+            
             Array.Sort(tab);
             int number;
-            int tmp;
-            number = Console.Read();
-            for (int i = 0; i < tab.Length; i++)
+            do
             {
-                if(number > tab[i])
+                Console.WriteLine("entrez un nombre");
+
+            } while (!int.TryParse(Console.ReadLine(), out number));
+            for (int i = 4; i >= 0; i--)
+            {
+  
+                if (number > tab[i] )
                 {
-                    for (int j = i; j < tab.Length; j++)
-                    {
-                        tmp = tab[j];
+                    
+                 for(int j = 5; j > i ; j--)
+                    {   
+                        
+                        tab[j] = tab[j - 1];
 
                     }
-                    
+                 tab[i + 1] = number;
+                    break;
                 }
             }
-            for (int i = 0; i < tab.Length; i++)
+            for (int i = 0; i < 6; i++)
             {
                 Console .WriteLine(tab[i]);
             }
